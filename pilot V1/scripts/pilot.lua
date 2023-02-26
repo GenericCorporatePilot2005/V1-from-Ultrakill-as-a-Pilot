130c8ceabb9ab99384f1ba3f0954eb86e8f5f028
NamesAreHard6600 <109428297+NamesAreHard6600@users.noreply.github.com>
2023-02-25 20:39:35 -0800

    Get Weapon Working
    Gets the weapon working:
    The init was not running and the weapon had a few small issues. Check the changes.

--- pilot V1/scripts/pilot.lua
+++ pilot V1/scripts/pilot.lua
@@ -27,47 +27,48 @@ modApi:appendAsset("img/portraits/pilots/Nico_Pilot_V1_blink.png", path .."img/p
 
 function this:init(mod)
 
-	require(mod.scriptPath .."libs/pilotSkill_tooltip").Add(pilot.Skill, PilotSkill("Blood Lust", "If an adjacent vek suffers damage, heals 1 health point.\nReplaces repair with a punch that flips an enemy's attack."))
+	--This line errors because libs/pilotSkill_tooltip doesn't exist. You'll want to fix that before uncommenting this
+	--require(mod.scriptPath .."libs/pilotSkill_tooltip").Add(pilot.Skill, PilotSkill("Blood Lust", "If an adjacent vek suffers damage, heals 1 health point.\nReplaces repair with a punch that flips an enemy's attack."))
+
+	replaceRepair:addSkill{
+		Name = "Parry",
+		Description = "Repairing is replaced with a melee attack that flips enemies' attacks.",
+		weapon = "Nico_V1skill",
+		pilotSkill = "Nico_V1skill",
+		Icon = "img/weapons/V1Punchrepair.png",
+		IsActive = function(pawn)
+			return pawn:IsAbility(pilot.Skill)
+		end
+	}
 
 	---- Parry ----
 	Nico_V1skill = Skill:new{
+		Name = "Parry",
+		Description = "A melee attack that flips enemies' attacks.",
 		Icon = "img/weapons/V1Punchrepair.png",
 		Flip = true,
-		PathSize = 1,
+		PathSize = 1, --This does the TargetArea on its own, no need for our own
 		Damage=1,
-		TipImage = {
+		TipImage = { --You'll have to create a custom tip image if you want the enemy to fire
 			Unit = Point(2,3),
 			Target = Point(2,2),
-		}
+			Enemy = Point(2,2),
+		} --Check Science_Confuse in weapons_science.lua if you want an example
 	}
 
 
 	function Nico_V1skill:GetSkillEffect(p1, p2)
 		local ret = SkillEffect()
+		local direction = GetDirection(p2-p1)
 		local push_damage = self.Flip and DIR_FLIP or direction
-		local damage = SpaceDamage(target, self.Damage, push_damage)
+		local damage = SpaceDamage(p2, self.Damage, push_damage)
 		damage.sAnimation = "punch1_"..direction
 		ret:AddMelee(p2 - DIR_VECTORS[direction], damage)
 		return ret
 	end
-
-	function Nico_V1skill:GetTargetArea(point) 
-  	  local ret = PointList()
-		ret:push_back(curr)    
-	    return ret 
-	end
 end
 
 
-	
-replaceRepair:addSkill{
-	Name = "Parry",
-	Description = "Repairing is replaced with a melee attack that flips enemies' attacks.",
-	weapon = "Nico_V1skill",
-	pilotSkill = "Nico_V1skill",
-	Icon = "img/weapons/V1Punchrepair.png",
-	IsActive = function(pawn)
-		return pawn:IsAbility(pilot.Skill)
-	end
-}
+
+
 return this
